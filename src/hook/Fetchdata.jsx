360f287b33ec433012c773bd08a82496342cf91e
Sabayas <sabayasmin345@gmail.com>
2026-03-15 23:37:15 +0530

    developing fetch data component to fetch data from pokeapi and display it on the screen

--- src/hook/Fetchdata.jsx
+++ src/hook/Fetchdata.jsx
@@ -33,41 +33,75 @@
 
 // export default Fetchdata;
 
-import React, { useEffect } from 'react' 
-import { useState } from 'react';
+// import React, { useEffect } from 'react' 
+// import { useState } from 'react';
 
 
+
+// const Fetchdata = () => {
+
+//   const [data, SetData] = useState([]);
+
+// // fetch data from the api function
+//   const fetchpokemon = () => {
+
+//   fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+//   .then((res) => res.json())
+//   .then((data) =>{
+//   SetData(data);
+//   })
+//   .catch((err) => console.log(err));
+
+// }
+
+// console.log(data);
+
+// // useEffect to call the fetchpokemon function when the component mounts
+// useEffect(() => {
+//   fetchpokemon();
+// }, []);
+
+// // condition to render the data if it exists
+// if (data) {
+//   return (
+//     <div><h1>{data.name}</h1>
+//   </div>
+//   )
+// }
+// };
+
+// export default Fetchdata;
+
+import React, { use, useState } from 'react'
+import { useEffect } from 'react';
 
 const Fetchdata = () => {
 
   const [data, SetData] = useState([]);
+   
+  const fetchdata = () => {
+      fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => {
+        SetData(json);
+        console.log(json);
+      })
+    }
 
-// fetch data from the api function
-  const fetchpokemon = () => {
 
-  fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
-  .then((res) => res.json())
-  .then((data) =>{
-  SetData(data);
-  })
-  .catch((err) => console.log(err));
 
-}
+      console.log(data);
 
-console.log(data);
-
-// useEffect to call the fetchpokemon function when the component mounts
-useEffect(() => {
-  fetchpokemon();
-}, []);
-
-// condition to render the data if it exists
-if (data) {
+      useEffect(() => {
+        fetchdata();
+      }, []);
   return (
-    <div><h1>{data.name}</h1>
-  </div>
+    <div>
+      <h1>Fetch Data</h1>
+      <h1>{data.id}</h1>
+      <p>{data.title}</p>
+    </div>
   )
 }
-};
 
-export default Fetchdata;
+export default Fetchdata
